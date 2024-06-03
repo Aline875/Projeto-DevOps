4735e3c6e60a7e4b589aaf3f22c2e69f55bf393f
Aline <113183673+Aline875@users.noreply.github.com>
2024-06-03 15:25:04 -0300

    .

--- backend/script.js
+++ backend/script.js
@@ -5,25 +5,25 @@ const form = document.querySelector('form');
 form.addEventListener('submit', (event) => {
   event.preventDefault();
 
-  // Captura os dados do formulário
+  
   const nome = document.getElementById('nome').value.trim();
   const email = document.getElementById('email').value.trim();
   const senha = document.getElementById('senha').value.trim();
   const confirmarSenha = document.getElementById('confirmarSenha').value.trim();
 
-  // Verifica se os campos obrigatórios foram preenchidos
+  
   if (!nome || !email || !senha || !confirmarSenha) {
     alert('Preencha todos os campos.');
     return;
   }
 
-  // Verifica se as senhas coincidem
+ 
   if (senha !== confirmarSenha) {
     alert('As senhas não coincidem.');
     return;
   }
 
-  // Cria um objeto com os dados do formulário
+  
   const dados = {
     nome,
     email,
@@ -31,7 +31,7 @@ form.addEventListener('submit', (event) => {
     confirmarSenha
   };
 
-  // Envia os dados para a API usando Axios
+  
   axios.post('http://localhost:3000/cadastro', dados)
     .then(response => {
       console.log('Cadastro realizado com sucesso!');
